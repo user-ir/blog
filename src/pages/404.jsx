0220ac23f367931b884af96617843ccb853f6381
user-ir <81340105+user-ir@users.noreply.github.com>
2021-11-24 15:16:35 +1100

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>Unfortunately we couldn't find what you were looooking for :(</p>
   </Layout>
 )
 
